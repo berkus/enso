830184443d3f8fe3f206f9693fb62389e6a93ec4
sirpengi <sirpengi@8b7adc99-b347-0410-ae0a-d9e86c8d69b5>
2008-09-02 09:35:13 +0000

    Applied patch to make windows show on monitor with focus in Linux

--- branches/community/enso/graphics/__init__.py
+++ branches/community/enso/graphics/__init__.py
@@ -4,8 +4,14 @@ _graphics = enso.providers.getInterface( "graphics" )
 
 from enso.graphics.measurement import pointsToPixels, pixelsToPoints
 
+def getDesktopOffset():
+  left, top = _graphics.getDesktopOffset()
+  left = pixelsToPoints(left)
+  top = pixelsToPoints (top)
+  return (left, top)
+
 def getDesktopSize():
-    width, height = _graphics.getDesktopSize()
-    width = pixelsToPoints( width )
-    height = pixelsToPoints( height )
-    return ( width, height )
+  width, height = _graphics.getDesktopSize()
+  width = pixelsToPoints(width)
+  height = pixelsToPoints(height)
+  return (width, height)
